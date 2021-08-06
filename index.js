68973687263b6206143a6f09d45c6fc194c327da
Martin Moy <martinmoy1991@gmail.com>
2021-08-06 17:56:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+ 
+function writeCards(names, event) {
+  let thankYouCards = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(
+      `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return thankYouCards;
+}
+
+function countDown(x) {
+  for (let countdown = x; countdown >= 0; countdown--) {
+console.log(countdown);}
+}
